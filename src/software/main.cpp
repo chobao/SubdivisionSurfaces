890280eaa6a15f035834f83f5a4ca6c7d6b86bd5
BC <b757508036@126.com>
2021-01-28 11:15:43 +0800

    move glsl file path as a argument of program

--- src/software/main.cpp
+++ src/software/main.cpp
@@ -12,16 +12,16 @@ void Run(const CommonUtils::Config& config) {
 
 
 int main(int argc, const char* argv[]){
-    if(argc != 2) {
-        std::cout<<"Please enter mainSubdivisionSurface [obj_path]";
+    if(argc != 4) {
+        std::cout<<"Please enter mainSubdivisionSurface [obj_path] [vertex_shader_path] [fragment_shader_path]\n";
         return 0;
     }
 
      CommonUtils::Config config;
     config.view_width = 800, config.view_height = 600;
     config.model_path = argv[1];
-    config.vertex_shader_path = "/home/baochong/Projects/HomeWork/GeometryModeling/SubdivisionSurface/src/visualization/model_v.glsl";
-    config.fragment_shader_path = "/home/baochong/Projects/HomeWork/GeometryModeling/SubdivisionSurface/src/visualization/model_f.glsl";
+    config.vertex_shader_path = argv[2];
+    config.fragment_shader_path = argv[3];
     config.maximum_level = 3;
     Run(config);
     //test3dOrdering();

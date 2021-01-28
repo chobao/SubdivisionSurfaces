614ebb429cde12b19a3931e8d78609df23b36d43
BC <b757508036@126.com>
2021-01-28 12:34:02 +0800

    refine the scripts for user input

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#declare -a array=('./res/monsterfrog.obj')
+INPUT_MODEL=$1
+declare -a array=(${INPUT_MODEL})
 res_dir="./res"
-array=(`ls ${res_dir}/*.obj`) # visualization all models
+#array=(`ls ${res_dir}/*.obj`) # visualization all models
 arraylength=${#array[@]}
 exe_path="./build/src/mainSubdivisionSurface"
 vertex_shader="./src/visualization/model_v.glsl"
@@ -11,3 +12,4 @@ do
         echo ${exe_path} ${array[${i}]} ${vertex_shader} ${fragment_shader}
         ${exe_path} ${array[${i}]} ${vertex_shader} ${fragment_shader}
 done
+
